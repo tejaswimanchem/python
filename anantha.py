cbd1274399d8deaeb1ad8f971e5947115472ac28
tejaswimanchem <61531073+tejaswimanchem@users.noreply.github.com>
2020-02-27 11:04:40 +0530

    Add files via upload

---
+++ anantha.py
@@ -0,0 +1,19 @@
+def evenorodd(n):
+    if n%2==0:
+        print("given",n,"is even")
+    else:
+        print("given",n,"is odd number")
+        
+def evenorodd_N(n):
+    for i in range(1,n):
+        if i%2==0:
+            print (i)
+
+def PoS(n):
+    if n>0:
+        print("given",n,"is even")
+    else:
+        print("given",n,"is odd number")
+        
+        
+        
